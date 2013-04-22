e49dd494631f1cb7894b544fe4c2f6d5814ce275
q45 <quintinsmith@icloud.com>
2013-04-22 13:57:58 -0600

    add comment seciont, partial, db

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ActiveRecord::Base
+	#allow for mass assignment
+	attr_accessible :author_name, :body
+
+	belongs_to :article
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130422163956) do
+ActiveRecord::Schema.define(version: 20130422194328) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.integer  "article_id"
+    t.string   "author_name"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
